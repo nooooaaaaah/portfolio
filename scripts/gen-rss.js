a024b6b3a58437bca7a7131dfdafaa8a5772058a
Noah Fence <nspielman@live.com>
2024-03-18 04:02:04 -0600

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Noah Spielman',
+    site_url: 'https://portfolio-gamma-dusky-42.vercel.app',
+    feed_url: 'https://portfolio-gamma-dusky-42.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
